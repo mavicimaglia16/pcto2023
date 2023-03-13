80ad5245044455e00e0b06893b1161dd0c77bbff
mavicimaglia16 <127745281+mavicimaglia16@users.noreply.github.com>
2023-03-13 16:42:40 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int  numero = 16;
+double numerolungo = 23456;
+float numeroconlavirgola = 23.4 ; 
+char carattere = 'c'; 
+printf(" il numero e': %d\n", numero );
+printf("numero lungo e: %f,numero con la virgola e': %f/n" , numerolungo, numeroconlavirgola);
+printf("il carattere e'%c\n", carattere); 
+}
