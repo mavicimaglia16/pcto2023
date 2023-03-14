5ed82fd197d72b6aecdbb0cba0518da5e53eec75
mavicimaglia16 <127745281+mavicimaglia16@users.noreply.github.com>
2023-03-14 10:14:02 +0100

    Add files via upload

--- esempionuovo.c
+++ esempionuovo.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 int main ()
+
 {
 
 int eta; 
+char sesso;
 printf( "quanti anni hai ? ");
 scanf("%d",&eta);
-printf("la tua eta e' %d ",eta); 
+
+printf("quale e'il tuo sesso ");
+scanf(" %c",&sesso);
+printf("io ho %d anni\n e sono %c\n", eta , sesso );
+
+
+
 
 
 
